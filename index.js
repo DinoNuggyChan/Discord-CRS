a6d61b1fb7a7c59714108e5e07d07e2eaa8e29bd
Nugget~ ♡ <68508971+Oni-Chan-inc@users.noreply.github.com>
2021-04-23 05:34:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+  
+/**
+ * @name SteamAppLinks
+ * @author Oni-Chan-inc
+ * @version 0.0.1
+ * @description Opens Steam Links from people's Profiles and in discord in Steam app
+ * @website https://waa.ai/nugget
+ * @source https://github.com/Oni-Chan-inc/SteamAppLinks
+ */
+
 var rpc = require("discord-rpc")
 const client = new rpc.Client({ transport: 'ipc' })
 
